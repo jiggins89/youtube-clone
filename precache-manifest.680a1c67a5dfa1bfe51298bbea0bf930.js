744990a4d3dd755b05025f7911eb2d32e85b98bc
John Higgins <johnhiggins@outlook.ie>
2019-06-07 19:00:54 +0100

    Updates

---
+++ precache-manifest.680a1c67a5dfa1bfe51298bbea0bf930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99595259ca93f6cbf20d81906fae52f",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "9bd2ef760de15191a3c7",
+    "url": "/youtube-clone/static/css/main.ff2057c6.chunk.css"
+  },
+  {
+    "revision": "e3432fbede1e9a69ec3e",
+    "url": "/youtube-clone/static/js/2.16c02a56.chunk.js"
+  },
+  {
+    "revision": "9bd2ef760de15191a3c7",
+    "url": "/youtube-clone/static/js/main.f9765ec8.chunk.js"
+  },
+  {
+    "revision": "7c348438a34bf75c231e",
+    "url": "/youtube-clone/static/js/runtime~main.a1cbaaee.js"
+  }
+]);
